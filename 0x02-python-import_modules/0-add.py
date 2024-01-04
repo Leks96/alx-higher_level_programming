be4798f813e2075b55b3a834a34bef4897fc775f
Saidi Sodiq <saidisodiq96@gamil.com>
2024-01-04 20:08:33 +0100

    simple import to add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# 0-add.py
+# Saidi Sodiq <saidisodiq92gmail.com>
+# import add function from add_0.py
+from add_0 import add
+# assign value
+a, b = 1, 2
+# print result
+print("{} + {} = {}".format(a, b, add(a,b)))
