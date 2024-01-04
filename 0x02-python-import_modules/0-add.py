907cc6a1af766c412e0dfaabe5973cea04b80277
Saidi Sodiq <saidisodiq96@gamil.com>
2024-01-04 20:15:30 +0100

    fixed indentation errors

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 # 0-add.py
 # Saidi Sodiq <saidisodiq92gmail.com>
-# import add function from add_0.py
-from add_0 import add
+
+# condition before import
+
+if __name__ == "__main__":
+	"""print the sum of 1 and 2"""
+	from add_0 import add
 # assign value
-a, b = 1, 2
+	a, b = 1, 2
 # print result
-print("{} + {} = {}".format(a, b, add(a,b)))
+	print("{} + {} = {}".format(a, b, add(a,b)))
