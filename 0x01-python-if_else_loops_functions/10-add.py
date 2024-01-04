d3a632adf9d693636a0d3cdf9d02186f1827f74e
Saidi Sodiq <saidisodiq96@gamil.com>
2024-01-04 05:22:51 +0100

    initial commit

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+# 10-add.py
+# Saidi Sodiq <saidisodiq96>
+
+def add(a, b):
+	"""adds two integers and returns the result."""
+	return a + b
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 11-pow.py
+# Saidi Sodiq <saidisodiq96@gamil.com>
+
+def pow(a, b):
+	"""computes a to the power of b and return the value."""
+	return a ** b 
