8f259b02090b7f67ad7fa413aae53e7422186d3f
Saidi Sodiq <saidisodiq96@gamil.com>
2024-01-07 10:07:31 +0100

    custom calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -10,8 +10,14 @@ if __name__ == "__main__":
 	a = 10
 	b = 5
 
+# perform the calculations
+	add_calc = add(a, b)
+	sub_calc = sub(a, b)
+	mul_calc = mul(a, b)
+	div_calc = div(a, b)
+
 #display results
-	print("{} + {} = {}".format(a, b, add(a, b)))
-	print("{} - {} = {}".format(a, b, sub(a, b)))
-	print("{} * {} = {}".format(a, b, mul(a, b)))
-	print("{} / {} = {}".format(a, b, div(a, b)))
+	print("{} + {} = {}".format(a, b, add_calc))
+	print("{} - {} = {}".format(a, b, sub_calc))
+	print("{} * {} = {}".format(a, b, mul_calc))
+	print("{} / {} = {}".format(a, b, div_calc))
