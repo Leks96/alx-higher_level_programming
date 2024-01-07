58d309b90ff05dd4ea6045c39af38d40399be73a
Saidi Sodiq <saidisodiq96@gamil.com>
2024-01-07 17:29:11 +0100

    script: to print name defined

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-import dis, marshal
-
-def get_names(file_path):
-	"""prints all the names defined by the compiled module hidden_4.pyc"""
-	with open(file_path, 'rb') as file:
-		code_object = marshal.load(file)
-		return [name for name in code_object.co_names if not name.startswith('__')]
-
+import hidden_4
+"""prints all the names defined by the compiled module hidden_4.pyc"""
 
 if __name__ == "__main__":
-	file_path = 'hidden_4.pyc'
-	names = get_names(file_path)
+	def_names = dir(hidden_4)
 
-
-	for name in sorted(names):
-		print(name)
+	for names in sorted(def_names):
+		if not name.startswith("__"):
+			print(name)
