1b657f0454bc94de642bdfedae58db2ed0efeb82
Saidi Sodiq <saidisodiq96@gamil.com>
2024-01-12 21:50:17 +0100

    script: replace search with replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,6 @@
 def search_replace(my_list, search, replace):
 	"""replaces all occurrences of an element by another in a new list."""
 	new_list = []
-
 	for i in my_list:
 		new_list.append(replace if i == search else i)
-
 	return new_list
