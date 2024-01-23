06e14b07845e1f04f591f8c027507873ff4b3059
Saidi Sodiq <saidisodiq96@gamil.com>
2024-01-23 17:05:21 +0100

    script: priny x el of list int only

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+	"""prints the first x elements of a list and only integers."""
+	count = 0
+	try:
+		for i in range(x):
+			value = my_list[i]
+			if isinstance(value, int):
+				print("{:d}".format(value),end='')
+				count += 1
+	except (IndexError, TypeError, ValueError):
+		print()
+		print("Value of x is more than the length of list")
+	
+	print()
+	return count
