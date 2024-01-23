0e8f8484ed8308874c1d24c3042db6c817850b7a
Saidi Sodiq <saidisodiq96@gamil.com>
2024-01-23 16:45:17 +0100

    script: print type error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,10 +3,6 @@ def safe_print_integer(value):
 	"""prints an integer with "{:d}".format()."""
 	try:
 		print("{:d}".format(value))
-		print()
-		
 		return True
-
-	except: (ValueError, TypeError)
-
-		return false
+	except (ValueError, TypeError):
+		return False
