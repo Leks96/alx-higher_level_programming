2c400aff988771367b874b6292078eeeb3e98af0
Saidi Sodiq <saidisodiq96@gamil.com>
2024-01-25 04:22:57 +0100

    oop: size of class square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""Defines a square class"""
+
+class Square:
+	"""Reps the class"""
+
+	def __init__(self, size):
+		"""Inits a square
+
+		Args:
+			size (): size of the square.
+		"""
+		self.__size = size
