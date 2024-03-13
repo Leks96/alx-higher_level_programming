97da8768a2915b51496e4bf6efea4bc6fa921e90
Saidi Sodiq <saidisodiq96@gamil.com>
2024-03-13 17:55:46 +0100

    script: full creation updated

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -12,10 +12,11 @@
 --      id = 3, name = “Bob”, score = 14
 --      id = 4, name = “George”, score = 8
 
+USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS second_table (id INT,
 name VARCHAR(256),
 score INT);
-INSERT INTO second_table (id, name, score)
+INSERT INTO second_table
 VALUES (1, "John", 10),
 (2, "Alex", 3),
 (3, "Bod", 14),
