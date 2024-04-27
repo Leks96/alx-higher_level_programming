fef4c32c62f6419d50d0a8a1b4fac8b7f109f0cf
Saidi Sodiq <saidisodiq96@gamil.com>
2024-04-27 14:04:37 +0100

    fixed check fails

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
-# Check if URL argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-        exit 1
-	fi
-
-# URL provided as argument
-	URL=$1
-
-# Send request using curl and get the size of the response body in bytes
-	SIZE=$(curl -s -w '%{size_download}' -o /dev/null "$URL")
-
-# Display the size of the response body
-	echo "Size of the response body: $SIZE bytes"
-
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
