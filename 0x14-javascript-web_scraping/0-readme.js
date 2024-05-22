84ec2de26d11d3e72dd9bb9f7264af40d781d842
Saidi Sodiq <saidisodiq96@gamil.com>
2024-05-22 17:29:11 +0100

    initial commit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-// Import the built-in Node.js 'fs' module.
 const fs = require('fs');
-
 fs.readFile(process.argv[2], 'utf8', (error, content) => {
   if (error) {
     console.error('Error reading the file:', error);
