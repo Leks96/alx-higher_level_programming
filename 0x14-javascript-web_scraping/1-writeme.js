fa7b43918917f226f71a5be137891bb2a02aeb88
Saidi Sodiq <saidisodiq96@gamil.com>
2024-05-22 17:38:34 +0100

    initial commit

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
-fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
 	if (err) {
 		console.error(err);
-	} else {
-		console.log('file written successfully');
 	}
 });
