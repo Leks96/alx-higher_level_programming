a85363f38ecdf0cce65f1cabe30042777cad988b
Saidi Sodiq <saidisodiq96@gamil.com>
2024-05-22 17:11:23 +0100

    initial commit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,21 +1,18 @@
 #!/usr/bin/node
-//a script that reads and prints the content of a file.
 
 const fs = require('fs');
 // Import the built-in Node.js 'fs' module.
 
-const file = process.argv[2];
-
-fs.readFile(file, 'utf8', (err, res) => {
+fs.readFile(process.argv[2], 'utf8', (error, content) => {
   // Use fs.readFile() to read the contents of a file specified as a command-line argument
   // 'utf8' specifies the encoding of the file being read
 
-  if (err) {
+  if (error) {
     // If an error occurs during the file read operation, the 'error' parameter will contain an error object.
-    console.error(err);
+    console.error('Error reading the file:', error);
 
   } else {
     // If the file is read successfully, the 'content' parameter will contain the contents of the file as a string.
-    console.log(res);
+    console.log(content);
   }
 });
