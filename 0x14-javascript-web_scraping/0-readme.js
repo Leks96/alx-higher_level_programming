eb0c0010c4dd8601be2941fa02214bce5fba4ab3
Saidi Sodiq <saidisodiq96@gamil.com>
2024-05-22 15:59:01 +0100

    fixed checker error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,20 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
+// Import the built-in Node.js 'fs' module.
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log(data)
-	}
+fs.readFile(filePath, 'utf8', function (error, content) {
+  // Use fs.readFile() to read the contents of a file specified as a command-line argument
+  // 'utf8' specifies the encoding of the file being read
+
+  if (error) {
+    // If an error occurs during the file read operation, the 'error' parameter will contain an error object.
+    console.error('Error reading the file:', error);
+
+  } else {
+    // If the file is read successfully, the 'content' parameter will contain the contents of the file as a string.
+    console.log(content);
+  }
 });
